4da3c5abbc75b53953290c11df79b3be13d6c4b9
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-06-05 22:51:03 -0300

    feat: Heap - Ceil, Floor

--- src/main/java/adt/heap/extended/FloorCeilHeapImpl.java
+++ src/main/java/adt/heap/extended/FloorCeilHeapImpl.java
@@ -12,14 +12,48 @@ public class FloorCeilHeapImpl extends HeapImpl<Integer> implements FloorCeilHea
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (int i = 0; i < array.length; i++) {
+			this.insert(array[i]);
+		}
+
+		return floorAux(numero, null);
+	}
+
+	private Integer floorAux(double taget, Integer floor) {
+		Integer root = this.extractRootElement();
+
+		if (root != null) {
+			if (taget >= root && (floor == null || root >= floor)) {
+				floor = this.floorAux(taget, root);
+			} else {
+				floor = this.floorAux(taget, floor);
+			}
+		}
+
+		return floor;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (int i = 0; i < array.length; i++) {
+			this.insert(array[i]);
+		}
+
+		return this.floorAux(numero, null);
+	}
+
+	private Integer ceilAux(double target, Integer ceil) {
+		Integer root = this.extractRootElement();
+
+		if (root != null) {
+			if (target <= root && (ceil == null || root <= ceil)) {
+				ceil = this.ceilAux(target, root);
+			} else {
+				ceil = this.ceilAux(target, ceil);
+			}
+		}
+
+		return ceil;
 	}
 
 }
